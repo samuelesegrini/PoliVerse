04e151bc17a0b46302d27bbe207d0dd67be3c352
Samuele Segrini <samuele.segrini@mail.polimi.it>
2023-10-18 18:39:45 +0200

    Initial Commit

---
+++ PoliVerse/PoliVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliVerseApp.swift
+//  PoliVerse
+//
+//  Created by Samuele Segrini on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PoliVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
